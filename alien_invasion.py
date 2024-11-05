bbbeb26bce0c19da6f77f0b27a05f4056e6e70f2
GeorgeKnyazkov <weemercury009@yandex.ru>
2024-11-05 16:46:55 +0300

    added alien_invasion.py, ship.py, settings.py, modified .gitignore

---
+++ alien_invasion.py
@@ -0,0 +1,41 @@
+import sys 
+
+import pygame
+
+from settings import Settings
+
+
+class AlienInvasion:
+    """Класс для управления ресурсами и поведением игры."""
+    
+    def __init__(self):
+        """Инициализирует игру и создает игровые ресурсы."""
+        pygame.init()
+        self.settings = Settings()
+        
+        self.screen = pygame.display.set_mode(
+            (self.settings.screen_width, self.settings.screen_height))
+        pygame.display.set_caption("Alien Invasion")
+        
+        # Назначение цвета фона.
+        # self.bg_color = (230, 230, 230)
+        
+    def run_game(self):
+        """Запуск основного цикла игры."""
+        while True:
+            # Отслеживаем события клавиатуры и мыши
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+            
+            # При каждом проходе цикла перерисовывается экран.
+            self.screen.fill(self.settings.bg_color)
+                    
+            # Отображение последнего прорисованного экрана.
+            pygame.display.flip()
+            
+            
+if __name__ == '__main__':
+    # Создание экземпляра и запуск игры
+    ai = AlienInvasion()
+    ai.run_game()
