428f36908dcbecfc29f634a025778826561fb740
GeorgeKnyazkov <weemercury009@yandex.ru>
2024-11-27 22:30:38 +0300

    added shot limit, _update_bullets(refactoring)

--- alien_invasion.py
+++ alien_invasion.py
@@ -35,15 +35,9 @@ class AlienInvasion:
             # Отслеживаем события клавиатуры и мыши через метод _check_events
             self._check_events()
             self.ship.update()
-            self.bullets.update()
+            self._update_bullets()
             self._update_screen()
-            
-            # Удаление снарядов вышедших за край экрана
-            for bullet in self.bullets.copy():
-                if bullet.rect.bottom <= 0:
-                    self.bullets.remove(bullet)
-            # print(len(self.bullets))
-            
+
     def _check_events(self):
         """Обрабатывает нажатие клавиш и события мыши.""" 
         # Отслеживаем события клавиатуры и мыши
@@ -77,8 +71,20 @@ class AlienInvasion:
             
     def _fire_bullet(self):
         """Создание нового снаряда и включение его в группу bullets."""
-        new_bullet = Bullet(self)
-        self.bullets.add(new_bullet)
+        if len(self.bullets) < self.settings.bullets_allowed:
+            new_bullet = Bullet(self)
+            self.bullets.add(new_bullet)
+            
+    def _update_bullets(self):
+        """Обновляет позиции снарядов и уничтожает старые снаряды."""
+        # Обновление позиций снарядов
+        self.bullets.update()
+        
+        # Удаление снарядов вышедших за край экрана
+        for bullet in self.bullets.copy():
+            if bullet.rect.bottom <= 0:
+                self.bullets.remove(bullet)
+        # print(len(self.bullets))
             
     def _update_screen(self):
         """Обновляет изображения на экране и отображает новый экран."""
--- settings.py
+++ settings.py
@@ -9,11 +9,12 @@ class Settings():
         self.bg_color = (37, 40, 80)
         
         # Настройки корабля
-        self.ship_speed = 1.5
+        self.ship_speed = 8
         
         # Параметры снарядов
-        self.bullet_speed = 1 
+        self.bullet_speed = 8
         self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_color = (0, 0, 0)
+        self.bullets_allowed = 3
         
