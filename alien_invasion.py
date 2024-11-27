78f5d2f8c52b19c39ad5fb0f76a6cd1b820363b7
GeorgeKnyazkov <weemercury009@yandex.ru>
2024-11-27 21:25:16 +0300

    added bullet.py

--- alien_invasion.py
+++ alien_invasion.py
@@ -14,9 +14,12 @@ class AlienInvasion:
         pygame.init()
         self.settings = Settings()
         
-        self.screen = pygame.display.set_mode((0,0), pygame.FULLSCREEN)
-        self.settings.screen_width = self.screen.get_rect().width
-        self.settings.screen_height = self.screen.get_rect().height
+        self.screen = pygame.display.set_mode(
+            (self.settings.screen_width, self.settings.screen_height))
+        """Полноэкранный режим"""
+        # self.screen = pygame.display.set_mode((0,0), pygame.FULLSCREEN)
+        # self.settings.screen_width = self.screen.get_rect().width
+        # self.settings.screen_height = self.screen.get_rect().height
         pygame.display.set_caption("Alien Invasion")
         
         self.ship = Ship(self)
