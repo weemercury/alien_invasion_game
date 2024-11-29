5a82a84c6ad47c90bfd713b6f2bc5111c26fa081
GeorgeKnyazkov <weemercury009@yandex.ru>
2024-11-29 17:25:08 +0300

    refactoring def _create_fleet() and added  def _create_alien()

--- alien_invasion.py
+++ alien_invasion.py
@@ -91,7 +91,7 @@ class AlienInvasion:
         # print(len(self.bullets))
         
     def _create_fleet(self):
-        """Создание флота вторжения."""
+        """Создание флота пришельцев."""
         # Создание пришельца и вычисление кол-ва пришельцев в ряду.
         # Интервал между соседними пришельцами равен ширине пришельца.
         alien = Alien(self)
@@ -101,11 +101,15 @@ class AlienInvasion:
         
         # Создание первого ряда пришельцев.
         for alien_number in range(number_aliens_x):
-            # Создание пришельца и размещение его в ряду.
-            alien = Alien(self)
-            alien.x = alien_width + 2 * alien_width * alien_number
-            alien.rect.x = alien.x
-            self.aliens.add(alien)
+            self._create_alien(alien_number)
+    
+    def _create_alien(self, alien_number):
+        """Создание пришельца и размещение его в ряду."""
+        alien = Alien(self)
+        alien_width = alien.rect.width
+        alien.x = alien_width + 2 * alien_width * alien_number
+        alien.rect.x = alien.x
+        self.aliens.add(alien)
             
     def _update_screen(self):
         """Обновляет изображения на экране и отображает новый экран."""
