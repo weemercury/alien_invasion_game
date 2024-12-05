b70860fc3ed276b65296e9f7449b56e6ae2c4e05
GeorgeKnyazkov <weemercury009@yandex.ru>
2024-12-05 16:40:03 +0300

    create start/end game, added def _check_play_button(), change def _check_events()

--- alien_invasion.py
+++ alien_invasion.py
@@ -65,6 +65,28 @@ class AlienInvasion:
                 self._check_keydown_events(event)
             elif event.type == pygame.KEYUP:
                 self._check_keyup_events(event)
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                mouse_pos = pygame.mouse.get_pos()
+                self._check_play_button(mouse_pos)
+                
+    def _check_play_button(self, mouse_pos):
+        """Запускает новую игру при нажатии кнопки Play."""
+        button_clicked = self.play_button.rect.collidepoint(mouse_pos)
+        if button_clicked and not self.stats.game_active:
+            # Сброс игровой статистики.
+            self.stats.reset_stats()
+            self.stats.game_active = True
+            
+            # Очистка списков пришельцев и снарядов.
+            self.aliens.empty()
+            self.bullets.empty()
+            
+            # Создание нового флота и размещение корабля в центре.
+            self._create_fleet()
+            self.ship.center_ship()
+            
+            # Указатель мыши скрывается.
+            pygame.mouse.set_visible(False)
     
     def _check_keydown_events(self, event):
         """Реагирует на нажатие клавиш."""
@@ -149,6 +171,7 @@ class AlienInvasion:
             sleep(0.5)
         else:
             self.stats.game_active = False
+            pygame.mouse.set_visible(True)
         
     def _check_aliens_bottom(self):
         """Проверяет, добрались ли пришельцы до нижнего края экрана."""
