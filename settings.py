2ede6c96941102370703a33fc90de3fdc38e958d
GeorgeKnyazkov <weemercury009@yandex.ru>
2024-12-05 13:55:46 +0300

    change self.fleet_drop_speed

--- settings.py
+++ settings.py
@@ -21,6 +21,6 @@ class Settings():
         
         # Настройки пришельцев.
         self.alien_speed = 1.0
-        self.fleet_drop_speed = 100
+        self.fleet_drop_speed = 10
         # fleet_direction = 1 - движение вправо; а -1 - влево
         self.fleet_direction = 1
