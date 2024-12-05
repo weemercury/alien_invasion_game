181dc0faef3ce490c56dabf562f96ffdefe8c9ee
GeorgeKnyazkov <weemercury009@yandex.ru>
2024-12-05 13:53:30 +0300

    make game_active = False

--- game_stats.py
+++ game_stats.py
@@ -6,8 +6,8 @@ class GameStats():
         self.settings = ai_game.settings
         self.reset_stats()
         
-        # Игра запускается в активном состоянии.
-        self.game_active = True
+        # Игра запускается в неактивном состоянии.
+        self.game_active = False
         
     def reset_stats(self):
         """Инициализирует статистику, изменяющуюся в ходе игры."""
