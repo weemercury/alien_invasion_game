54e7ebdf871d52b9fd3e46d35f1d956c67ed5200
GeorgeKnyazkov <weemercury009@yandex.ru>
2024-12-06 18:16:53 +0300

    added best/record score

--- alien_invasion.py
+++ alien_invasion.py
@@ -142,6 +142,7 @@ class AlienInvasion:
             for aliens in collisions.values():
                 self.stats.score += self.settings.alien_points * len(aliens)
             self.sb.prep_score()
+            self.sb.check_high_score()
         
         if not self.aliens:
             # Уничтожение существующих снарядов и создание нового флота.
--- scoreboard.py
+++ scoreboard.py
@@ -14,8 +14,15 @@ class Scoreboard():
         self.text_color = (30, 30, 30)
         self.font = pygame.font.SysFont(None, 36)
         
-        # Подготовка исходного изображения.
+        # Подготовка изображений счетов.
         self.prep_score()
+        self.prep_high_score()
+        
+    def check_high_score(self):
+        """Проверяет, появился ли новый рекорд."""
+        if self.stats.score > self.stats.high_score:
+            self.stats.high_score = self.stats.score
+            self.prep_high_score()
         
     def prep_score(self):
         """Преобразует текущий счет в графическое изображение."""
@@ -29,6 +36,18 @@ class Scoreboard():
         self.score_rect.right = self.screen_rect.right - 20
         self.score_rect.top = self.screen_rect.top + 20
         
+    def prep_high_score(self):
+        """Преобразует рекордный счет в графическое изображение."""
+        high_score = round(self.stats.high_score, -1)
+        high_score_str = "{:,}".format(high_score)
+        self.high_score_image = self.font.render(high_score_str, True, 
+                    self.text_color, self.settings.bg_color)
+        # Рекордный счет выравнивается по центру верхней части экрана.
+        self.high_score_rect = self.high_score_image.get_rect()
+        self.high_score_rect.centerx = self.screen_rect.centerx
+        self.high_score_rect.top = self.high_score_rect.top + 20
+        
     def show_score(self):
         """Вывод счет на экран."""
         self.screen.blit(self.score_image, self.score_rect)
+        self.screen.blit(self.high_score_image, self.high_score_rect)
